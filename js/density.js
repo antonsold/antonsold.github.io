3eba646b3e12d06f2c858423d043b46fc912c6b3
antonsold <soldatenkovanton@mail.ru>
2020-05-27 23:06:30 +0200

    asdsvwv4

--- js/density.js
+++ js/density.js
@@ -81,7 +81,7 @@ class DensityMapPlot {
                 });
                 tooltip.classed('hidden', false)
                     .attr('style', 'left:' + (100 + mouse[0]) +
-                        'px; top:' + (d3.event.pageY + 200) +"px")
+                        'px; top:' + (d3.event.pageY + 60000) +"px")
                     .html(d.properties[name_property] + ": " + d.properties[feature]);
             })
             .on('mouseout', function() {
